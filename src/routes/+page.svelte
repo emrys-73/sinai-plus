026a0115c86eba0cc81ccf9f6c9b490b8f8fc6a8
Adrian Montoya <montoya@astralta.com>
2024-12-24 15:12:04 +0100

    Added addresses

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,74 +7,71 @@
 
 
     let showAlert = false;
+    let alertMessage = "";
+
+    let next_house_number = 0;
 
 
     const houses = [
         {
             id: generateUUID(),
-            name: "House 1",
-            address: "Address line 1, 567897 Munich",
-            url: "https://www.google.com/maps/place/Biserica+Lui+Dumnezeu+Elim+M%C3%BCnchen/@48.187656,11.5841412,16z/data=!3m1!5s0x479e743b45b8b5a3:0x99d400c2da8f424c!4m6!3m5!1s0x479e743b45bfffff:0xd137c2e364737a7f!8m2!3d48.1883574!4d11.5837768!16s%2Fg%2F11fp1t5bn0?entry=ttu&g_ep=EgoyMDI0MTIxMS4wIKXMDSoASAFQAw%3D%3D",
+            name: "Biserica Elim München",
+            address: "Ingolstädterstraße 43, 80807 München",
+            url: "https://www.google.com/maps/place/Ingolst%C3%A4dter+Str.+43,+80807+M%C3%BCnchen/@48.1883574,11.5811965,17z/data=!3m1!4b1!4m6!3m5!1s0x479e743b45959529:0x452765343d96!8m2!3d48.1883574!4d11.5837768!16s%2Fg%2F11b8v53f9q?entry=ttu&g_ep=EgoyMDI0MTIxMS4wIKXMDSoASAFQAw%3D%3D",
             active: true,
             isStart: true,
             isEnd: false,
             visited: false,
-            current: false
+            current: false,
+            time_to_next: 25,
         },
         {
             id: generateUUID(),
-            name: "House 2",
-            address: "Address line 1, 567897 Munich",
-            url: "https://www.google.com/maps/place/Biserica+Lui+Dumnezeu+Elim+M%C3%BCnchen/@48.187656,11.5841412,16z/data=!3m1!5s0x479e743b45b8b5a3:0x99d400c2da8f424c!4m6!3m5!1s0x479e743b45bfffff:0xd137c2e364737a7f!8m2!3d48.1883574!4d11.5837768!16s%2Fg%2F11fp1t5bn0?entry=ttu&g_ep=EgoyMDI0MTIxMS4wIKXMDSoASAFQAw%3D%3D",
+            name: "Kirchenstraße 15, 82194 Gröbenzell",
+            address: "Kirchenstraße 15, 82194 Gröbenzell",
+            url: "https://www.google.com/maps/place/Kirchenstra%C3%9Fe+15,+82194+Gr%C3%B6benzell/@48.1938593,11.370283,17z/data=!4m6!3m5!1s0x479e78d795d67319:0x3ee55bffe0d6ab3b!8m2!3d48.1938593!4d11.3728579!16s%2Fg%2F11c20y32vg?entry=ttu&g_ep=EgoyMDI0MTIxMS4wIKXMDSoASAFQAw%3D%3D",
             active: true,
             isStart: true,
             isEnd: false,
             visited: false,
-            current: false
+            current: false,
+            time_to_next: 25,
         },
         {
             id: generateUUID(),
-            name: "House 1",
-            address: "Address line 1, 567897 Munich",
-            url: "https://www.google.com/maps/place/Biserica+Lui+Dumnezeu+Elim+M%C3%BCnchen/@48.187656,11.5841412,16z/data=!3m1!5s0x479e743b45b8b5a3:0x99d400c2da8f424c!4m6!3m5!1s0x479e743b45bfffff:0xd137c2e364737a7f!8m2!3d48.1883574!4d11.5837768!16s%2Fg%2F11fp1t5bn0?entry=ttu&g_ep=EgoyMDI0MTIxMS4wIKXMDSoASAFQAw%3D%3D",
+            name: "Pretzfelderstraße 47,81249 München",
+            address: "Pretzfelderstraße 47,81249 München",
+            url: "https://www.google.com/maps/place/Pretzfelder+Str.+47,+81249+M%C3%BCnchen/@48.1519886,11.4084161,16z/data=!3m1!4b1!4m6!3m5!1s0x479e7877ef6856c5:0xcedbd9b7cd421f20!8m2!3d48.1519886!4d11.4109964!16s%2Fg%2F11bw4326w3?entry=ttu&g_ep=EgoyMDI0MTIxMS4wIKXMDSoASAFQAw%3D%3D",
             active: true,
             isStart: true,
             isEnd: false,
             visited: false,
-            current: false
+            current: false,
+            time_to_next: 25,
         },
         {
             id: generateUUID(),
-            name: "House 1",
-            address: "Address line 1, 567897 Munich",
-            url: "https://www.google.com/maps/place/Biserica+Lui+Dumnezeu+Elim+M%C3%BCnchen/@48.187656,11.5841412,16z/data=!3m1!5s0x479e743b45b8b5a3:0x99d400c2da8f424c!4m6!3m5!1s0x479e743b45bfffff:0xd137c2e364737a7f!8m2!3d48.1883574!4d11.5837768!16s%2Fg%2F11fp1t5bn0?entry=ttu&g_ep=EgoyMDI0MTIxMS4wIKXMDSoASAFQAw%3D%3D",
+            name: "Alpenstraße 12, 81541 München",
+            address: "Alpenstraße 12, 81541 München",
+            url: "https://www.google.com/maps/place/Alpenstra%C3%9Fe+12,+81541+M%C3%BCnchen/@48.102479,11.5484255,11.72z/data=!4m6!3m5!1s0x479ddf725107f187:0x423a4e383f639492!8m2!3d48.1168399!4d11.5824558!16s%2Fg%2F11nl59t80c?entry=ttu&g_ep=EgoyMDI0MTIxMS4wIKXMDSoASAFQAw%3D%3D",
             active: true,
             isStart: true,
             isEnd: false,
             visited: false,
-            current: false
+            current: false,
+            time_to_next: 25,
         },
         {
             id: generateUUID(),
-            name: "House 1",
-            address: "Address line 1, 567897 Munich",
-            url: "https://www.google.com/maps/place/Biserica+Lui+Dumnezeu+Elim+M%C3%BCnchen/@48.187656,11.5841412,16z/data=!3m1!5s0x479e743b45b8b5a3:0x99d400c2da8f424c!4m6!3m5!1s0x479e743b45bfffff:0xd137c2e364737a7f!8m2!3d48.1883574!4d11.5837768!16s%2Fg%2F11fp1t5bn0?entry=ttu&g_ep=EgoyMDI0MTIxMS4wIKXMDSoASAFQAw%3D%3D",
+            name: "Georg-Eisenreich-Straße 6, 85540 Haar",
+            address: "Georg-Eisenreich-Straße 6, 85540 Haar",
+            url: "https://www.google.com/maps/place/Georg-Eisenreich-Stra%C3%9Fe+6,+85540+Haar/@48.1091485,11.7260009,17z/data=!3m1!4b1!4m6!3m5!1s0x479de1e5fecf6b91:0x382a9943d7c56156!8m2!3d48.1091485!4d11.7285812!16s%2Fg%2F11nxbn8bn1?entry=ttu&g_ep=EgoyMDI0MTIxMS4wIKXMDSoASAFQAw%3D%3D",
             active: true,
             isStart: true,
             isEnd: false,
             visited: false,
-            current: false
-        },
-        {
-            id: generateUUID(),
-            name: "House 1",
-            address: "Address line 1, 567897 Munich",
-            url: "https://www.google.com/maps/place/Biserica+Lui+Dumnezeu+Elim+M%C3%BCnchen/@48.187656,11.5841412,16z/data=!3m1!5s0x479e743b45b8b5a3:0x99d400c2da8f424c!4m6!3m5!1s0x479e743b45bfffff:0xd137c2e364737a7f!8m2!3d48.1883574!4d11.5837768!16s%2Fg%2F11fp1t5bn0?entry=ttu&g_ep=EgoyMDI0MTIxMS4wIKXMDSoASAFQAw%3D%3D",
-            active: true,
-            isStart: true,
-            isEnd: false,
-            visited: false,
-            current: false
+            current: false,
+            time_to_next: 25,
         },
 
     ]
@@ -150,6 +147,8 @@
     ]
 
 
+
+
 //     Start 19:20 Ingolstädterstraße 43, 80807 München
 // 20:00 - 20:20 Birkenstraß 22, 85419 Mauern 
 // 20:45 - 21:15 Waldkirchener Straße 9, 84030 Landshut 
@@ -168,33 +167,32 @@
 
 </script>
 
-
 <div class="bg-[#1C1C1C] w-full min-h-screen flex flex-col justify-start items-center text-white px-4 py-6 gap-6">
-    <div class=" text-xl font-semibold uppercase w-full flex items-start justify-start py-0">
+    <div class="text-xl font-semibold uppercase w-full flex items-start justify-start py-0">
         Sinai
     </div>
     {#if showAlert}
         <div class="py-4 w-full px-4 bg-primary flex flex-col text-black font-black rounded-md">
-            Alert message
+            Mesaj de alertă
         </div>
     {/if}
 
-    <div class="flex flex-col gap-0 justify-center items-center w-full">
-        <span class="text-[#959595] font-bold justify-center items-start flex flex-col w-full">
-            Nächster Halt
+    <div class="flex flex-col gap-0 justify-center items-center w-full py-6 bg-primary bg-opacity-100 text-black rounded-xl px-4">
+        <span class="text-black font-semibold opacity-60 justify-center items-start flex flex-col w-full">
+            Stația următoare
         </span>
-        <span class="font-bold text-lg flex flex-col items-start justify-center w-full text-white">
-            Haus 4
+        <span class="font-bold text-2xl flex flex-col items-start justify-center w-full text-black">
+            {houses[next_house_number].name}
         </span>
     </div>
 
     <div class="w-full flex flex-row gap-4 justify-center items-center">
-        <Button class="w-full px-4 py-2 rounded-full text-black font-bold hover:px-16 transition-all duration-300 ease-in-out hover:font-bold">
-            Get Directions
+        <Button href={houses[next_house_number].url} class="w-full px-4 py-2 rounded-full text-black font-bold hover:px-16 transition-all duration-300 ease-in-out hover:font-bold">
+            Afișează ruta
         </Button>
 
-        <Button class="w-full px-4 py-2 bg-transparent border-[1px] border-primary font-bold hover:font-bold rounded-full hover:text-black hover:px-16 transition-all duration-300 ease-in-out">
-            Download map
+        <Button href="/muc.pdf" class="w-full px-4 py-2 bg-transparent border-[1px] border-primary font-bold hover:font-bold rounded-full hover:text-black hover:px-16 transition-all duration-300 ease-in-out">
+            Descarcă harta
         </Button>
     </div>
 
@@ -205,11 +203,11 @@
     <div class="w-full flex flex-col justify-center items-center py-2">
         <div class="w-full flex flex-row justify-around items-center">
             <span class="font-bold text-lg flex flex-col items-start justify-center w-full text-white">
-                Houses
+                Case
             </span>
 
-            <Button class="w-full px-4 py-2 bg-transparent border-[1px] border-primary font-bold hover:font-black rounded-full hover:text-black hover:px-16 transition-all duration-300 ease-in-out">
-                Karte runterladen
+            <Button href="/muc.pdf" class="w-full px-4 py-2 bg-transparent border-[1px] border-primary font-bold hover:font-bold rounded-full hover:text-black hover:px-16 transition-all duration-300 ease-in-out">
+                Descarcă harta
             </Button>
         </div>
 
@@ -218,7 +216,7 @@
             <table class="w-full justify-center text-white rounded-lg overflow-hidden drop-shadow-xl">
                 <thead>
                   <tr class="bg-secondary text-black font-bold hidden">
-                    <th class="py-1 px-4 text-start">Name</th>
+                    <th class="py-1 px-4 text-start">Nume</th>
                   </tr>
                 </thead>
                 <tbody>
@@ -239,11 +237,11 @@
     <div class="w-full flex flex-col justify-center items-center py-2">
         <div class="w-full flex flex-row justify-around items-center">
             <span class="font-bold text-lg flex flex-col items-start justify-center w-full text-white">
-                Lieder
+                Cântări
             </span>
 
-            <Button class="w-full px-4 py-2 bg-transparent border-[1px] border-primary font-bold hover:font-black rounded-full hover:text-black hover:px-16 transition-all duration-300 ease-in-out">
-                Alle Lieder runterladen
+            <Button class="w-full px-4 py-2 hidden bg-transparent border-[1px] border-primary font-bold hover:font-black rounded-full hover:text-black hover:px-16 transition-all duration-300 ease-in-out">
+                Descarcă toate cântecele
             </Button>
         </div>
 
@@ -252,15 +250,15 @@
             <table class="w-full justify-center text-white rounded-lg overflow-hidden drop-shadow-xl">
                 <thead>
                   <tr class="bg-secondary text-black font-bold hidden">
-                    <th class="py-1 px-4 text-start">Name</th>
+                    <th class="py-1 px-4 text-start">Nume</th>
                   </tr>
                 </thead>
                 <tbody>
-                  {#each houses as h, index}
+                  {#each songs as s, index}
                     <tr class="{index % 2 === 0 ? 'bg-[#303030]' : 'bg-[#262626]'} hover:bg-primary hover:text-black hover:font-bold">
                         <td class="py-2 px-4 border-none hover:py-4 transition-all ease-in-out duration-200">
-                            <a href={h.url}>
-                                {h.name}
+                            <a href={s.url}>
+                                {s.name}
                             </a>
                         </td>
                     </tr>
