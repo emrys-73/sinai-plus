1dbc23b99c8f1f4709593994649d4f7214a08206
Adrian Montoya <montoya@astralta.com>
2024-12-25 12:58:17 +0100

    LA Ready + UX update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,7 +9,7 @@
     let showAlert = false;
     let alertMessage = "";
 
-    let next_house_number = 4;
+    let next_house_number = 0;
 
 
     const houses = [
@@ -75,6 +75,112 @@
         },
 
     ]
+
+
+    const houses_2 = [
+        {
+            id: generateUUID(),
+            name: "Biserica Elim München",
+            address: "Ingolstädterstraße 43, 80807 München",
+            url: "https://www.google.com/maps/place/Ingolst%C3%A4dter+Str.+43,+80807+M%C3%BCnchen/@48.1883574,11.5811965,17z/data=!3m1!4b1!4m6!3m5!1s0x479e743b45959529:0x452765343d96!8m2!3d48.1883574!4d11.5837768!16s%2Fg%2F11b8v53f9q?entry=ttu&g_ep=EgoyMDI0MTIxMS4wIKXMDSoASAFQAw%3D%3D",
+            active: true,
+            isStart: true,
+            isEnd: false,
+            visited: false,
+            current: false,
+            time_to_next: 25,
+        },
+        {
+            id: generateUUID(),
+            name: "Birkenstraß 22, 85419 Mauern",
+            url: "https://www.google.com/maps/place/Birkenstra%C3%9Fe+22,+85419+Mauern/@48.4180679,11.3220027,9.68z/data=!4m6!3m5!1s0x479e394721bc53f3:0xf6c4fa56679f8371!8m2!3d48.519619!4d11.8974285!16s%2Fg%2F11csj3_7ff?entry=ttu&g_ep=EgoyMDI0MTIxMS4wIKXMDSoASAFQAw%3D%3D",
+            active: true,
+            isStart: true,
+            isEnd: false,
+            visited: false,
+            current: false,
+            time_to_next: 25,
+        },
+        {
+            id: generateUUID(),
+            name: "Waldkirchener Straße 9, 84030 Landshut",
+            url: "https://www.google.com/maps/place/Waldkirchener+Str.+9,+84030+Landshut/@48.560958,12.1400796,16z/data=!3m1!4b1!4m6!3m5!1s0x479e25f7fda95363:0x1c3d9673d278a89e!8m2!3d48.560958!4d12.1400796!16s%2Fg%2F11g4hzb8dx?entry=ttu&g_ep=EgoyMDI0MTIxMS4wIKXMDSoASAFQAw%3D%3D",
+            active: true,
+            isStart: true,
+            isEnd: false,
+            visited: false,
+            current: false,
+            time_to_next: 25,
+        },
+        {
+            id: generateUUID(),
+            name: "Schleienweg 5, 84174 Eching",
+            url: "https://www.google.com/maps/place/Schleienweg+5,+84174+Eching/@48.4964251,12.0344574,17z/data=!3m1!4b1!4m6!3m5!1s0x479e24a4228b5fe3:0x1cdf10d9aca04af1!8m2!3d48.4964251!4d12.0370377!16s%2Fg%2F11tg0qzccp?entry=ttu&g_ep=EgoyMDI0MTIxMS4wIKXMDSoASAFQAw%3D%3D",
+            active: true,
+            isStart: true,
+            isEnd: false,
+            visited: false,
+            current: false,
+            time_to_next: 25,
+        },
+        {
+            id: generateUUID(),
+            name: "Hauptstraße 21, 84172 Buch am Erlbach",
+            url: "https://www.google.com/maps/place/Hauptstra%C3%9Fe+21,+84172+Buch+am+Erlbach/@48.4443384,12.0321103,17z/data=!3m1!4b1!4m6!3m5!1s0x479e22fb4c02c18f:0x5945e37388455343!8m2!3d48.4443385!4d12.0369758!16s%2Fg%2F11j0k5d3dm?entry=ttu&g_ep=EgoyMDI0MTIxMS4wIKXMDSoASAFQAw%3D%3D",
+            active: true,
+            isStart: true,
+            isEnd: false,
+            visited: false,
+            current: false,
+            time_to_next: 25,
+        },
+        {
+            id: generateUUID(),
+            name: "Lilienweg 3, 84174 Eching",
+            url: "https://www.google.com/maps/place/Lilienweg+3,+84174+Eching/@48.4933986,12.0598688,16z/data=!3m1!4b1!4m6!3m5!1s0x479e248474569b31:0x8844afe9e573503a!8m2!3d48.4933986!4d12.0624491!16s%2Fg%2F11cscb__6f?entry=ttu&g_ep=EgoyMDI0MTIxMS4wIKXMDSoASAFQAw%3D%3D",
+            active: true,
+            isStart: true,
+            isEnd: false,
+            visited: false,
+            current: false,
+            time_to_next: 25,
+        },
+        {
+            id: generateUUID(),
+            name: "Schmiedleiten 84174 Haunwang",
+            url: "https://www.google.com/maps/place/Schmiedleiten,+84174+Eching/@48.4749446,12.0458553,17z/data=!3m1!4b1!4m6!3m5!1s0x479e236e0e52710b:0x849d2363652fd61!8m2!3d48.4749446!4d12.0484356!16s%2Fg%2F1tj1r1mb?entry=ttu&g_ep=EgoyMDI0MTIxMS4wIKXMDSoASAFQAw%3D%3D",
+            active: true,
+            isStart: true,
+            isEnd: false,
+            visited: false,
+            current: false,
+            time_to_next: 25,
+        },
+        {
+            id: generateUUID(),
+            name: "Hauptstraße 10, 84174 Eching Viecht",
+            url: "https://www.google.com/maps/place/Hauptstra%C3%9Fe+10,+84174+Eching/@48.4789413,12.0032659,12.08z/data=!4m6!3m5!1s0x479e248fbd29b853:0x1fbd923d893b829c!8m2!3d48.4996869!4d12.0628555!16s%2Fg%2F11c2dnln95?entry=ttu&g_ep=EgoyMDI0MTIxMS4wIKXMDSoASAFQAw%3D%3D",
+            active: true,
+            isStart: true,
+            isEnd: false,
+            visited: false,
+            current: false,
+            time_to_next: 25,
+        },
+        {
+            id: generateUUID(),
+            name: "Eichenstraße 8, 84174 Eching",
+            url: "https://www.google.com/maps/place/Eichenstra%C3%9Fe+8,+84174+Eching/@48.5010033,12.064321,17z/data=!3m1!4b1!4m6!3m5!1s0x479e248ed02ed037:0xe91df8d7435303ac!8m2!3d48.5010033!4d12.0669013!16s%2Fg%2F11c24kr4w0?entry=ttu&g_ep=EgoyMDI0MTIxMS4wIKXMDSoASAFQAw%3D%3D",
+            active: true,
+            isStart: true,
+            isEnd: false,
+            visited: false,
+            current: false,
+            time_to_next: 25,
+        },
+
+    ]
+
 
     const songs = [
         {
@@ -158,7 +264,7 @@
             id: generateUUID(),
             name: "Hört wie die Hirten singen",
             address: "Address line 1, 567897 Munich",
-            url: "/Hört wie die Hirten singen.pdf",
+            url: "/hirten.pdf",
             active: true,
             isStart: true,
             isEnd: false,
@@ -233,7 +339,7 @@
         },
         {
             id: generateUUID(),
-            name: "Veniti sa-l cantam",
+            name: "Feliz Navidad! (Veniti sa-l cantam)",
             address: "Address line 1, 567897 Munich",
             url: "/Veniti sa-l cantam.pdf",
             active: true,
@@ -244,8 +350,6 @@
         },
 
     ]
-
-
 
 
 //     Start 19:20 Ingolstädterstraße 43, 80807 München
@@ -281,22 +385,22 @@
             Stația următoare
         </span>
         <span class="font-bold text-2xl flex flex-col items-start justify-center w-full text-black">
-            {houses[next_house_number].name}
+            {houses_2[next_house_number].name}
         </span>
     </div>
 
     <div class="w-full flex flex-row gap-4 justify-center items-center">
-        <Button href={houses[next_house_number].url} class="w-full px-4 py-2 rounded-full text-black font-bold hover:px-16 transition-all duration-300 ease-in-out hover:font-bold">
+        <Button href={houses_2[next_house_number].url} class="w-full px-4 py-2 rounded-full text-black font-bold hover:px-16 transition-all duration-300 ease-in-out hover:font-bold">
             Afișează ruta
         </Button>
 
-        <Button href="/muc.pdf" class="w-full px-4 py-2 bg-transparent border-[1px] border-primary font-bold hover:font-bold rounded-full hover:text-black hover:px-16 transition-all duration-300 ease-in-out">
+        <Button href="/la.pdf" class="w-full px-4 py-2 bg-transparent border-[1px] border-primary font-bold hover:font-bold rounded-full hover:text-black hover:px-16 transition-all duration-300 ease-in-out">
             Descarcă harta
         </Button>
     </div>
 
     <div class="w-full flex flex-col justify-center items-center py-2 pt-4">
-        <img src="/muc.jpeg" alt="" class="w-full h-full rounded-xl">
+        <img src="/la.jpeg" alt="" class="w-full h-full rounded-xl">
     </div>
 
     <div class="w-full flex flex-col justify-center items-center py-2">
@@ -305,7 +409,7 @@
                 Case
             </span>
 
-            <Button href="/muc.pdf" class="w-full px-4 py-2 bg-transparent border-[1px] border-primary font-bold hover:font-bold rounded-full hover:text-black hover:px-16 transition-all duration-300 ease-in-out">
+            <Button href="/la.pdf" class="w-full px-4 py-2 bg-transparent border-[1px] border-primary font-bold hover:font-bold rounded-full hover:text-black hover:px-16 transition-all duration-300 ease-in-out">
                 Descarcă harta
             </Button>
         </div>
@@ -319,10 +423,10 @@
                   </tr>
                 </thead>
                 <tbody>
-                  {#each houses as h, index}
+                  {#each houses_2 as h, index}
                     <tr class="{index % 2 === 0 ? 'bg-[#303030]' : 'bg-[#262626]'} hover:bg-primary hover:text-black hover:font-bold">
-                        <td class="py-2 px-4 border-none hover:py-4 transition-all ease-in-out duration-200">
-                            <a href={h.url}>
+                        <td class="border-none">
+                            <a href={h.url} class="w-full h-full flex flex-col justify-center items-start py-2 px-4 hover:py-4 transition-all ease-in-out duration-200">
                                 {h.name}
                             </a>
                         </td>
@@ -355,8 +459,8 @@
                 <tbody>
                   {#each songs as s, index}
                     <tr class="{index % 2 === 0 ? 'bg-[#303030]' : 'bg-[#262626]'} hover:bg-primary hover:text-black hover:font-bold">
-                        <td class="py-2 px-4 border-none hover:py-4 transition-all ease-in-out duration-200">
-                            <a href={s.url}>
+                        <td class="border-none">
+                            <a href={s.url} class="py-2 px-4 hover:py-4 transition-all ease-in-out duration-200 w-full h-full flex flex-col justify-center items-start">
                                 {s.name}
                             </a>
                         </td>
